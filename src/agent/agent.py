83dfcecbd6f2f555cce92a8d5cf4dcbcc396eb8e
Getlar <tamastheactual@gmail.com>
2023-03-09 22:51:49 +0100

    Initial state, and required files

---
+++ src/agent/agent.py
@@ -0,0 +1,17 @@
+import gym
+import minerl
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+env = gym.make('MineRLBasaltFindCave-v0')
+obs = env.reset()
+
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    action["ESC"] = 0
+    obs, reward, done, _ = env.step(action)
+    env.render()
