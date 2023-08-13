a8381d2d4c37e1003efe64e34573429dc9b4e1d0
Getlar <tamastheactual@gmail.com>
2023-08-13 22:46:50 +0200

    Fixed Dockerfiles and added running scripts.

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,31 @@
-docker build --platform linux/x86_64 -t ranuon98/luxai_cpu -f Dockerfile.CPU .
+#!/bin/bash
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 <os> <device> <version>"
+    echo "Arguments:"
+    echo "  os      : Operating system (mac/windows)"
+    echo "  device  : Device type (CPU/GPU)"
+    echo "  version : Docker version tag"
+    exit 0
+fi
+
+if [ "$#" -ne 3 ]; then
+    echo "Error: Incorrect number of arguments. Use '$0 --help' for usage information."
+    exit 1
+fi
+
+if [ "$1" = "mac" ]; then
+    if [ "$2" = "CPU" ]; then
+        docker build --platform linux/x86_64 -t ranuon98/luxai_cpu:"$3" -f Dockerfile.CPU .
+    else
+        docker build --platform linux/x86_64 -t ranuon98/luxai_gpu:"$3" -f Dockerfile.GPU .
+    fi
+else
+    if [ "$2" = "CPU" ]; then
+        docker build -t ranuon98/luxai_cpu:"$3" -f Dockerfile.CPU .
+    else
+        docker build -t ranuon98/luxai_gpu:"$3" -f Dockerfile.GPU .
+    fi
+fi
+
+
--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,20 @@
-docker run -it -p 8888:8888 -v $(pwd)/src:/usr/src/luxai ranuon98/luxai_cpu /bin/bash
+#!/bin/bash
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 <device> <version>"
+    echo "Arguments:"
+    echo "  device  : Device type (CPU/GPU)"
+    echo "  version : Docker version tag"
+    exit 0
+fi
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Incorrect number of arguments. Use '$0 --help' for usage information."
+    exit 1
+fi
+
+if [ "$1" = "CPU" ]; then
+    docker run -it -p 8888:8888 -v $(pwd)/src:/usr/src/luxai ranuon98/luxai_cpu:"$2" /bin/bash
+else
+    docker run -it --gpus all -p 8888:8888 -v $(pwd)/src:/usr/src/luxai ranuon98/luxai_gpu:"$2" /bin/bash
+fi
