Fixed Invalid Action Masking

src/action/controllers.py
@@ -80,7 +80,8 @@ class SimpleUnitDiscreteController(Controller):
 
         self.env_cfg = env_cfg
         self.move_act_dims = 4
-        self.transfer_act_dims = 5 * 3
+        self.one_transfer_dim = 5
+        self.transfer_act_dims = self.one_transfer_dim * 3
         self.pickup_act_dims = 1
         self.dig_act_dims = 1
         self.recharge_act_dims = 1
@@ -225,12 +226,24 @@ class SimpleUnitDiscreteController(Controller):
         Doesn't account for whether robot has enough power
         """
 
+        ## Should return a 19x64x64 mask. 19 actions, 64x64 board
+        ## It should return all false where units are not there
+
         self.logger.debug(f"Creating simplified action mask for {agent}")
 
         shared_obs = obs[agent]
         factory_occupancy_map = (
             np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1
         )
+        player_occupancy_map = (
+            np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1
+        )
+        for player in shared_obs["units"]:
+            for unit_id in shared_obs["units"][player]:
+                unit = shared_obs["units"][player][unit_id]
+                pos = np.array(unit["pos"])
+                player_occupancy_map[pos[0], pos[1]] = unit["id"]
+                
         factories = {}
         for player in shared_obs["factories"]:
             factories[player] = {}
@@ -243,22 +256,25 @@ class SimpleUnitDiscreteController(Controller):
                 ] = f_data["strain_id"]
 
         units = shared_obs["units"][agent]
-        action_mask = np.zeros((self.total_act_dims), dtype=bool)
+        action_mask = np.zeros((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)
         for unit_id in units.keys():
-            action_mask = np.zeros(self.total_act_dims)
-            # movement is always valid
-            action_mask[:4] = True
+            
+            # get position of unit
+            pos = np.array(unit["pos"])
+
+            # move is only valid if there is a unit there
+            action_mask[:4, pos[0], pos[1]] = True
 
             # transferring is valid only if the target exists
             unit = units[unit_id]
-            pos = np.array(unit["pos"])
+            
             # a[1] = direction (0 = center, 1 = up, 2 = right, 3 = down, 4 = left)
             move_deltas = np.array([[0, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
             for i, move_delta in enumerate(move_deltas):
                 transfer_pos = np.array(
                     [pos[0] + move_delta[0], pos[1] + move_delta[1]]
                 )
-                # check if theres a factory tile there
+                # check if transfer position is valid
                 if (
                     transfer_pos[0] < 0
                     or transfer_pos[1] < 0
@@ -266,11 +282,20 @@ class SimpleUnitDiscreteController(Controller):
                     or transfer_pos[1] >= len(factory_occupancy_map[0])
                 ):
                     continue
+
+                # check if there is a unit there or factory
                 factory_there = factory_occupancy_map[transfer_pos[0], transfer_pos[1]]
+                unit_there = player_occupancy_map[transfer_pos[0], transfer_pos[1]]
+
+                if unit_there != -1:
+                    for j in range(3):  # we want to allow transfer of any kind of resouce if its possible
+                        index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
+                        action_mask[index, pos[0], pos[1]] = True
+
                 if factory_there in shared_obs["teams"][agent]["factory_strains"]:
-                    action_mask[
-                        self.transfer_dim_high - self.transfer_act_dims + i
-                    ] = True
+                    for j in range(3):  # same for factories
+                        index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
+                        action_mask[index, pos[0], pos[1]] = True
 
             factory_there = factory_occupancy_map[pos[0], pos[1]]
             on_top_of_factory = (
@@ -286,22 +311,22 @@ class SimpleUnitDiscreteController(Controller):
             )
             if board_sum > 0 and not on_top_of_factory:
                 action_mask[
-                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high
+                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
                 ] = True
 
             # pickup is valid only if on top of factory tile
             if on_top_of_factory:
                 action_mask[
-                    self.pickup_dim_high - self.pickup_act_dims : self.pickup_dim_high
+                    self.pickup_dim_high - self.pickup_act_dims : self.pickup_dim_high, pos[0], pos[1]
                 ] = True
                 action_mask[
-                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high
+                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
                 ] = False
 
-            # no-op is always valid
-            action_mask[-1] = True
-            break
-        
-        action_mask = np.zeros(shape=(19,)) == 1  # TODO: change this to dynamic size
+            # recharge should only be valid in the night
+            if shared_obs["step"] % 40 > 30:
+                action_mask[
+                    self.recharge_dim_high - self.recharge_act_dims : self.recharge_dim_high, pos[0], pos[1]
+                ] = True
     
         return action_mask

src/sb3_contrib/common/maskable/buffers.py
@@ -6,6 +6,8 @@ from gymnasium import spaces
 from stable_baselines3.common.buffers import DictRolloutBuffer, RolloutBuffer
 from stable_baselines3.common.type_aliases import TensorDict
 from stable_baselines3.common.vec_env import VecNormalize
+import logging
+logger = logging.getLogger(__name__)
 
 
 class MaskableRolloutBufferSamples(NamedTuple):
@@ -53,9 +55,12 @@ class MaskableRolloutBuffer(RolloutBuffer):
         n_envs: int = 1,
     ):
         super().__init__(buffer_size, observation_space, action_space, device, gae_lambda, gamma, n_envs)
+        logger.info(f"Creating {self.__class__.__name__}")
+        self.logger = logging.getLogger(f"{__name__}_{id(self)}")
         self.action_masks = None
 
     def reset(self) -> None:
+        logging.debug(f"Masks dim: {self.action_space}")
         if isinstance(self.action_space, spaces.Discrete):
             mask_dims = self.action_space.n
         elif isinstance(self.action_space, spaces.MultiDiscrete):
@@ -64,7 +69,6 @@ class MaskableRolloutBuffer(RolloutBuffer):
             mask_dims = 2 * self.action_space.n  # One mask per binary outcome
         else:
             raise ValueError(f"Unsupported action space {type(self.action_space)}")
-
         self.mask_dims = mask_dims
         self.action_masks = np.ones((self.buffer_size, self.n_envs, self.mask_dims), dtype=np.float32)
 
@@ -155,8 +159,11 @@ class MaskableDictRolloutBuffer(DictRolloutBuffer):
     ):
         self.action_masks = None
         super().__init__(buffer_size, observation_space, action_space, device, gae_lambda, gamma, n_envs=n_envs)
+        logger.info(f"Creating {self.__class__.__name__}")
+        self.logger = logging.getLogger(f"{__name__}_{id(self)}")
 
     def reset(self) -> None:
+        logging.debug(f"Masks dim: {self.action_space}")
         if isinstance(self.action_space, spaces.Discrete):
             mask_dims = self.action_space.n
         elif isinstance(self.action_space, spaces.MultiDiscrete):
@@ -167,7 +174,7 @@ class MaskableDictRolloutBuffer(DictRolloutBuffer):
             raise ValueError(f"Unsupported action space {type(self.action_space)}")
 
         self.mask_dims = mask_dims
-        self.action_masks = np.ones((self.buffer_size, self.n_envs, self.mask_dims), dtype=np.float32)
+        self.action_masks = np.ones((self.buffer_size, self.n_envs, self.mask_dims, 64, 64), dtype=np.float32)
 
         super().reset()
 
@@ -176,7 +183,8 @@ class MaskableDictRolloutBuffer(DictRolloutBuffer):
         :param action_masks: Masks applied to constrain the choice of possible actions.
         """
         if action_masks is not None:
-            self.action_masks[self.pos] = action_masks.reshape((self.n_envs, self.mask_dims))
+            
+            self.action_masks[self.pos] = action_masks.reshape((self.n_envs, self.mask_dims, 64, 64))
 
         super().add(*args, **kwargs)
 

src/stable_baselines3/common/torch_layers.py
@@ -165,8 +165,8 @@ class CustomExtractor(nn.Module):
         self.value_net = nn.Sequential(*value_net).to(device)
 
         # TODO: make these sizes dynamic
-        self.latent_dim_pi = 19  # action space
-        self.latent_dim_vf = 77824  # 19x64x64
+        self.latent_dim_pi = feature_dim  # action space
+        self.latent_dim_vf = 90112  # 19x64x64
 
     def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
         """