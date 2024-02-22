d893d2d4987eaaedb3d7a5d1372e96e117590cd9
gergely.m <m.gergely1999@gmail.com>
2024-02-22 16:01:21 +0000

    adjust main.py for model

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from argparse import Namespace
 from typing import Dict
 import torch.nn as nn
 from policy.net import Net
+from policy.simple_net import SimpleNet
 from kit.config import EnvConfig
 from impl_config import ModelParam, ActDims
 from kit.kit import (
@@ -22,7 +23,7 @@ import torch
 import numpy as np
 from player import Player
 ### The model path
-PATH = 'your/model/path.pth'
+PATH = 'model_360448.pth'
 ### DO NOT REMOVE THE FOLLOWING CODE ###
 agent_dict = (
     dict()
@@ -41,7 +42,7 @@ def agent_fn(observation, configurations):
     remainingOverageTime = observation.remainingOverageTime
     if step == 0:
         env_cfg = EnvConfig.from_dict(configurations["env_cfg"])
-        agent_dict[player] = Net()
+        agent_dict[player] = SimpleNet()
         agent_prev_obs[player] = dict()
         agent = agent_dict[player]
         agent.load_state_dict(torch.load(PATH,map_location=torch.device('cpu')))
@@ -66,8 +67,7 @@ def agent_fn(observation, configurations):
             valid_action = ActionParser().get_valid_actions(game_state, player_id)
             np2torch = lambda x, dtype: torch.tensor(x).unsqueeze(0).type(dtype)
             _,_,actions,_ = agent(torch.tensor(obs['global_feature'],dtype=torch.float).unsqueeze(0),
-                                  torch.tensor(obs['map_feature'],dtype=torch.float).unsqueeze(0),\
-                                tree.map_structure(lambda x: np2torch(x, torch.int16), obs['action_feature']),\
+                                  torch.tensor(obs['entity_feature'],dtype=torch.float).unsqueeze(0),\
                                 tree.map_structure(lambda x: np2torch(x, torch.bool), valid_action)
                                             )
             actions = tree.map_structure(lambda x: torch2np(x), actions)
@@ -107,6 +107,7 @@ if __name__ == "__main__":
         )
         if i==0:
             configurations = obs["info"]["env_cfg"]
+            print(configurations, file=sys.stderr)
         i += 1
         actions = agent_fn(observation, dict(env_cfg=configurations))
         # send actions to engine
