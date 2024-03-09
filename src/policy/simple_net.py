53601c80194e3e845dbd848410e5f5b211236c90
gergely.m <m.gergely1999@gmail.com>
2024-03-09 16:06:53 +0000

    add pooling layer

--- src/policy/simple_net.py
+++ src/policy/simple_net.py
@@ -24,7 +24,8 @@ class SimpleNet(nn.Module):
 
         self.large_embedding_dim = 8
         self.large_distance_embedding = nn.Sequential(
-            nn.Conv2d(self.map_feature_count, self.large_embedding_dim, kernel_size=7, stride=1, padding="same", bias=True),
+            nn.AvgPool2d(kernel_size=3, stride=1, padding="same"),
+            nn.Conv2d(self.map_feature_count, self.large_embedding_dim, kernel_size=5, stride=1, padding="same", bias=True),
             nn.LeakyReLU(),
         )
 
