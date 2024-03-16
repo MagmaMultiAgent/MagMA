c3ffceda40779508a75904962709aa6183b9e21b
gergely.m <m.gergely1999@gmail.com>
2024-03-16 16:05:16 +0000

    reduce map embedding im

--- src/policy/simple_net.py
+++ src/policy/simple_net.py
@@ -21,7 +21,7 @@ class SimpleNet(nn.Module):
         self.global_feature_count = 4
 
         self.map_feature_count = 3
-        self.map_embedding_dim = 4
+        self.map_embedding_dim = 2
         self.map_embedding = nn.Sequential(
             nn.Conv2d(self.map_feature_count, self.map_embedding_dim, kernel_size=1, stride=1, padding=0, bias=True),
             nn.LeakyReLU(),
