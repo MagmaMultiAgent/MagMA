8658b28261ac61b17949697c2f70f201036f91ef
gergely.m <m.gergely1999@gmail.com>
2024-04-25 19:27:32 +0000

    group by closest factory

--- src/parsers/feature_parser.py
+++ src/parsers/feature_parser.py
@@ -485,19 +485,14 @@ class FeatureParser():
     def get_unit_id(unit, factories):
         unit_id = int(unit.unit_id.split('_')[1])
         unit_id = 0
-        # 4 unit IDs based on map quadrant
-        H, W = 48, 48
-        x, y = unit.pos
-        if x < H // 2:
-            if y < W // 2:
-                unit_id = 0
-            else:
-                unit_id = 1
-        else:
-            if y < W // 2:
-                unit_id = 2
-            else:
-                unit_id = 3
+        # get closest factory
+        min_distance = sys.maxsize
+        for factory in factories:
+            distance = np.abs(np.array(factory.pos) - np.array(unit.pos)).sum()
+            if distance < min_distance:
+                min_distance = distance
+                unit_id = int(factory.unit_id.split('_')[1])
+
         unit_id += 10
         return unit_id
         
