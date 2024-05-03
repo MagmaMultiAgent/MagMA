Fixed this idiot masking issue oh my god

src/controller/controller.py
@@ -72,7 +72,7 @@ class MultiUnitController(Controller):
         self.env_cfg = env_cfg
         self.move_act_dims = 4
         self.one_transfer_dim = 5
-        self.transfer_act_dims = self.one_transfer_dim * 3
+        self.transfer_act_dims = self.one_transfer_dim * 2
         self.pickup_act_dims = 1
         self.dig_act_dims = 1
         self.recharge_act_dims = 1
@@ -113,10 +113,11 @@ class MultiUnitController(Controller):
 
     def _get_transfer_action(self, id):
         
-        id = id - (self.transfer_act_dims/3)
-        transfer_dir = id % (self.transfer_act_dims/3)
-        transfer_type_mapping = [0, 1, 4]  # Mapping index to desired value
-        transfer_type_index = id // (self.transfer_act_dims/3)  # 0 for ice, 1 for ore, 2 for power
+        id = id - (self.transfer_act_dims/2)
+        id += 1
+        transfer_dir = id % (self.transfer_act_dims/2)
+        transfer_type_mapping = [0, 1]  # Mapping index to desired value
+        transfer_type_index = id // (self.transfer_act_dims/2)  # 0 for ice, 1 for ore
         transfer_type = transfer_type_mapping[transfer_type_index.astype(int).item()]
         return np.array([1, transfer_dir, transfer_type, self.env_cfg.max_transfer_amount, 0, 1], dtype=int)
 
@@ -154,7 +155,7 @@ class MultiUnitController(Controller):
         """
         Converts the action id to a recharge action
         """
-        return np.array([4, 0, 0, 0, 0, 1])
+        return np.array([5, 0, 0, 0, 0, 1])
     
     def _is_light_unit_build_action(self, id):
         """
@@ -205,6 +206,7 @@ class MultiUnitController(Controller):
 
         unit_id = list(units.keys())
         factory_id = list(shared_obs["factories"][agent].keys())
+
         for unit_id, unit in units.items():
             pos = tuple(unit['pos'])
             filtered_action = action[pos]
@@ -231,6 +233,10 @@ class MultiUnitController(Controller):
             if not no_op:
                 lux_action[unit_id] = action_queue
 
+        
+
+        
+
         factories = shared_obs["factories"][agent]
         
         for factory_id, factory in factories.items():
@@ -245,7 +251,7 @@ class MultiUnitController(Controller):
                 lux_action[factory_id] = self._get_water_lichen_action(choice)
             else:
                 continue
-        
+
         return lux_action
 
     def action_masks(self, agent: str, obs: Dict[str, Any]):
@@ -287,7 +293,7 @@ class MultiUnitController(Controller):
             pos = np.array(unit["pos"])
 
             # move is only valid if there is a unit there
-            action_mask[:4, pos[0], pos[1]] = True
+            action_mask[:4, pos[0], pos[1]] = False
 
             # transferring is valid only if the target exists
             unit = units[unit_id]
@@ -313,12 +319,12 @@ class MultiUnitController(Controller):
                 if unit_there != -1:
                     for j in range(3):  # we want to allow transfer of any kind of resouce if its possible
                         index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
-                        action_mask[index, pos[0], pos[1]] = True
+                        action_mask[index, pos[0], pos[1]] = False
 
                 if factory_there in shared_obs["teams"][agent]["factory_strains"]:
                     for j in range(3):  # same for factories
                         index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
-                        action_mask[index, pos[0], pos[1]] = True
+                        action_mask[index, pos[0], pos[1]] = False
 
             factory_there = factory_occupancy_map[pos[0], pos[1]]
             on_top_of_factory = (
@@ -339,7 +345,7 @@ class MultiUnitController(Controller):
             if on_top_of_factory or factory_there != -1:
                 action_mask[
                     self.transfer_dim_high - self.one_transfer_dim : self.transfer_dim_high, pos[0], pos[1]
-                ] = False
+                ] = True
             # dig is valid only if on top of tile with rubble or resources or lichen
             board_sum = (
                 shared_obs["board"]["ice"][pos[0], pos[1]]
@@ -350,13 +356,13 @@ class MultiUnitController(Controller):
             if board_sum > 0 and not on_top_of_factory:
                 action_mask[
                     self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
-                ] = True
+                ] = False
 
             # pickup is valid only if on top of factory tile
             if on_top_of_factory:
                 action_mask[
                     self.pickup_dim_high - self.pickup_act_dims : self.pickup_dim_high, pos[0], pos[1]
-                ] = True
+                ] = False
                 action_mask[
                     self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
                 ] = False
@@ -365,16 +371,19 @@ class MultiUnitController(Controller):
             if shared_obs["real_env_steps"] % 40 > 30:
                 action_mask[
                     self.recharge_dim_high - self.recharge_act_dims : self.recharge_dim_high, pos[0], pos[1]
-                ] = True
+                ] = False
+
+            
+
+        
+
+
+        action_mask = np.ones((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)  
 
         for factory_id in factories:
             factory = factories[factory_id]
             pos = np.array(factory["pos"])
-            if factory["cargo"]["metal"] >= 10 and factory["power"] >= 50:
-                action_mask[self.light_unit_build_dim_high, pos[0], pos[1]] = True
-            elif factory["cargo"]["metal"] >= 50 and factory["power"] >= 500:
-                action_mask[self.heavy_unit_build_dim_high, pos[0], pos[1]] = True
-            elif factory["cargo"]["water"] >= 200:
-                action_mask[self.water_lichen_dim_high, pos[0], pos[1]] = True
+            action_mask[0:19, pos[0], pos[1]] = False
 
+        action_mask[4:14, :, :] = False
         return action_mask

src/net/mixed_net.py
@@ -397,7 +397,7 @@ class UNetWithResnet50Encoder(BaseFeaturesExtractor):
             nn.ConvTranspose2d(128, 50, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             # Final layer: Adjust channels to 25 without changing dimension
-            nn.ConvTranspose2d(50, 25, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(50, 20, kernel_size=3, stride=1, padding=1),
         )
         self.upconv_1 = nn.Conv2d(2048, 1024, kernel_size=1, stride=1)
         self.up_blocks = nn.ModuleList(up_blocks)

src/observation/observation_parser.py
@@ -158,31 +158,26 @@ class FeatureParser():
         # Global
 
         global_feature = {name: 0 for name in self.global_feature_names}
+
         # normalize between -1 and 1
         global_feature['env_step'] = (obs.real_env_steps - 0) / (env_cfg.max_episode_length - 0) * 2 - 1
-        # global_feature['cycle'] = obs.real_env_steps // env_cfg.CYCLE_LENGTH
-        # global_feature['hour'] = obs.real_env_steps % env_cfg.CYCLE_LENGTH
         hour = obs.real_env_steps % env_cfg.CYCLE_LENGTH
         global_feature['daytime_or_night'] = hour < 30
 
-        # global_feature['num_factory_own'] = (len(obs.factories[player]) - 0) / (env_cfg.MAX_FACTORIES - 0) * 2 - 1
-        # global_feature['num_factory_enm'] = (len(obs.factories[other_player]) - 0) / (env_cfg.MAX_FACTORIES - 0) * 2 - 1
-
         # Map
-
         map_feature = {name: np.zeros_like(obs.board.ice, dtype=np.float32) for name in self.map_featrue_names}
         map_feature['ice'] = obs.board.ice
         map_feature['ore'] = obs.board.ore
         map_feature['rubble'] = (obs.board.rubble - 0) / (env_cfg.MAX_RUBBLE - 0) * 2 - 1
 
         # Factory
-
         factory_feature = {name: np.zeros_like(obs.board.ice, dtype=np.float32) for name in self.factory_feature_names}
         for owner, factories in obs.factories.items():
             for fid, factory in factories.items():
                 x, y = factory.pos
 
-                factory_feature['factory_power'][x, y] = (factory.power - 0) / (heavy_cfg.BATTERY_CAPACITY - 0) * 2 - 1
+                ## TOASK: isnt this an issue since we are normalizing by heav cargo space?
+                factory_feature['factory_power'][x, y] = (factory.power - 0) / (heavy_cfg.CARGO_SPACE - 0) * 2 - 1
                 factory_feature['factory_ice'][x, y] = (factory.cargo.ice - 0) / (heavy_cfg.CARGO_SPACE - 0) * 2 - 1
                 factory_feature['factory_water'][x, y] = (factory.cargo.water - 0) / (heavy_cfg.CARGO_SPACE - 0) * 2 - 1
                 factory_feature['factory_ore'][x, y] = (factory.cargo.ore - 0) / (heavy_cfg.CARGO_SPACE - 0) * 2 - 1
@@ -199,7 +194,6 @@ class FeatureParser():
                             pass
 
         # Unit
-
         unit_feature = {name: np.zeros_like(obs.board.ice, dtype=np.float32) for name in self.unit_feature_names}
         units = obs.units[player]
         
@@ -226,16 +220,11 @@ class FeatureParser():
                 map_feature['enemy'][x + dx, y + dy] = 1.0
 
         # Assemble return
-
         global_feature = np.array(list(global_feature.values()))
         map_feature = np.array(list(map_feature.values()))
         factory_feature = np.array(list(factory_feature.values()))
         unit_feature = np.array(list(unit_feature.values()))
-
-        if output_dict:
-            return {'global_feature': global_feature, 'map_feature': map_feature, 'factory_feature': factory_feature, 'unit_feature': unit_feature}
-        
-        feature_map_size = (48, 48)
+        feature_map_size = (obs.board.ice.shape[0], obs.board.ice.shape[0])
         upscaled_features_global_features = np.tile(global_feature[:, np.newaxis, np.newaxis], (1, *feature_map_size))
 
         return upscaled_features_global_features, map_feature, factory_feature, unit_feature

src/wrappers/reward_wrapper.py
@@ -1,8 +1,8 @@
 from typing import Dict, List, Tuple, Union
-from rewards.ice_reward_parser import IceRewardParser
 import gym
 import copy
 import numpy as np
+from luxai_s2.state import StatsStateDict
 import kit.kit
 
 
@@ -18,44 +18,32 @@ class EarlyRewardParserWrapper(gym.Wrapper):
         """
         super().__init__(env)
         self.prev_step_metrics = None
-        self.reward_parser = IceRewardParser()
 
         self.global_info_names = [
-            'factory_count',
-            'unit_count',
-            'light_count',
-            'heavy_count',
-            'unit_ice',
-            'unit_ore',
-            'unit_water',
-            'unit_metal',
-            'unit_power',
-            'factory_ice',
-            'factory_ore',
-            'factory_water',
-            'factory_metal',
+            'power_consumed',
+            'water_consumed',
+            'metal_consumed',
+            'ore_transferred',
+            'ice_transferred',
+            'energy_pickup',
+            'rubble_destroyed',
+            'lichen_destroyed',
+            'ice_dug',
+            'ore_dug',
+            'metal_produced',
+            'water_produced',
+            'lichen_produced',
+            'light_robots_built',
+            'heavy_robots_built',
+            'light_power',
+            'heavy_power',
             'factory_power',
-            'total_ice',
-            'total_ore',
-            'total_water',
-            'total_metal',
             'total_power',
-            'lichen_count',
+            'action_queue_updates_success',
+            'action_queue_updates_total',
             'units_on_ice',
             'avg_distance_from_ice',
             'rubble_on_ice',
-
-            'ice_transfered',
-            'ore_transfered',
-            'ice_mined',
-            'ore_mined',
-            'lichen_grown',
-            'unit_created',
-            'light_created',
-            'heavy_created',
-            'unit_destroyed',
-            'light_destroyed',
-            'heavy_destroyed',
         ]
 
     def step(self, action):
@@ -76,89 +64,96 @@ class EarlyRewardParserWrapper(gym.Wrapper):
 
         obs = obs[agent]
 
-        metrics = self._get_info(agent, self.env.state, prev_obs=self.prev_step_metrics)
-        reward = self.reward_parser.parse(done, self.env.state, metrics)
+        metrics = self._get_info(agent, self.env.state.stats[agent], self.env.state)
+        reward = self.parse(self.env.state, metrics)
+
         self.prev_step_metrics = copy.deepcopy(metrics)
 
         info["metrics"] = metrics
         return obs, reward, termination[agent], truncation[agent], info
     
-    def _get_info(self, player: str, obs: kit.kit.GameState, prev_obs: kit.kit.GameState = None):
+    def _get_info(self, player: str, stats: StatsStateDict , state: kit.kit.GameState):
 
-        global_info = {k: 0 for k in self.global_info_names}
-        factories = list(obs.factories[player].values())
-        units = list(obs.units[player].values())
+        metrics = {k: 0 for k in self.global_info_names}
 
-        global_info['light_count'] = sum(int(u.unit_type == 'LIGHT') for u in units)
-        global_info['heavy_count'] = sum(int(u.unit_type == 'HEAVY') for u in units)
-        global_info['unit_count'] = global_info['light_count'] + global_info['heavy_count']
-        global_info["factory_count"] = len(factories)
+        units = list(state.units[player].values())
 
-        global_info['unit_ice'] = sum(u.cargo.ice for u in units)
-        global_info['unit_ore'] = sum(u.cargo.ore for u in units)
-        global_info['unit_water'] = sum(u.cargo.water for u in units)
-        global_info['unit_metal'] = sum(u.cargo.metal for u in units)
-        global_info['unit_power'] = sum(u.power for u in units)
+        
+        metrics["power_consumed"] = stats["consumption"]["power"]["HEAVY"] + stats["consumption"]["power"]["LIGHT"] + stats["consumption"]["power"]["FACTORY"]
+        metrics["water_consumed"] = stats["consumption"]["water"]
+        metrics["metal_consumed"] = stats["consumption"]["metal"]
 
-        global_info['factory_ice'] = sum(f.cargo.ice for f in factories)
-        global_info['factory_ore'] = sum(f.cargo.ore for f in factories)
-        global_info['factory_water'] = sum(f.cargo.water for f in factories)
-        global_info['factory_metal'] = sum(f.cargo.metal for f in factories)
-        global_info['factory_power'] = sum(f.power for f in factories)
+        
+        metrics["ore_transferred"] = stats["transfer"]["ore"]
+        metrics["ice_transferred"] = stats["transfer"]["ice"] ####
+        
+        metrics["energy_pickup"] = stats["pickup"]["power"]
 
-        global_info['total_ice'] = global_info['unit_ice'] + global_info['factory_ice']
-        global_info['total_ore'] = global_info['unit_ore'] + global_info['factory_ore']
-        global_info['total_water'] = global_info['unit_water'] + global_info['factory_water']
-        global_info['total_metal'] = global_info['unit_metal'] + global_info['factory_metal']
-        global_info['total_power'] = global_info['unit_power'] + global_info['factory_power']
-        global_info['lichen_count'] = np.sum(obs.board.lichen)
+        metrics["rubble_destroyed"] = stats["destroyed"]["rubble"]["LIGHT"] + stats["destroyed"]["rubble"]["HEAVY"] ##
+        metrics["lichen_destroyed"] = stats["destroyed"]["lichen"]["LIGHT"] + stats["destroyed"]["lichen"]["HEAVY"]
+
+        metrics["ice_dug"] = stats["generation"]["ice"]["HEAVY"] + stats["generation"]["ice"]["LIGHT"] #### 
+        metrics["ore_dug"] = stats["generation"]["ore"]["HEAVY"] + stats["generation"]["ore"]["LIGHT"]
+        metrics["metal_produced"] = stats["generation"]["metal"]
+        metrics["water_produced"] = stats["generation"]["water"] ####
+        metrics["lichen_produced"] = stats["generation"]["lichen"] 
+        metrics["light_robots_built"] = stats["generation"]["built"]["LIGHT"]
+        metrics["heavy_robots_built"] = stats["generation"]["built"]["HEAVY"]
+        metrics["light_power"] = stats["generation"]["power"]["LIGHT"]
+        metrics["heavy_power"] = stats["generation"]["power"]["HEAVY"]
+        metrics["factory_power"] = stats["generation"]["power"]["FACTORY"]
+        
+        metrics["action_queue_updates_success"] = stats["action_queue_updates_success"]
+        metrics["action_queue_updates_total"] = stats["action_queue_updates_total"]
 
         units_positions = [u.pos for u in units]
-        unit_board = np.zeros_like(obs.board.ice, dtype=np.bool8)
+        unit_board = np.zeros_like(state.board.ice, dtype=np.bool8)
         for pos in units_positions:
             unit_board[pos.x, pos.x] = True
-        ice_board = obs.board.ice
+        ice_board = state.board.ice
         units_standing_on_ice = [(ice_board[pos.x, pos.y] > 0) for pos in units_positions]
         unit_count_on_ice = sum(units_standing_on_ice)
-        global_info['units_on_ice'] = unit_count_on_ice
+        metrics['units_on_ice'] = unit_count_on_ice
 
         if len(units_positions) < 0:
             avg_distance_from_ice = 1.0
         else:
             avg_distance_from_ice = self.get_avg_distance(unit_board, ice_board)
-        global_info['avg_distance_from_ice'] = avg_distance_from_ice
+        metrics['avg_distance_from_ice'] = avg_distance_from_ice
 
-        ice_board = obs.board.ice.astype(np.float32)
-        rubble_board = obs.board.rubble.astype(np.float32)
+        ice_board = state.board.ice.astype(np.float32)
+        rubble_board = state.board.rubble.astype(np.float32)
         rubble_on_ice = ice_board * rubble_board
-        global_info['rubble_on_ice'] = np.sum(rubble_on_ice)
+        metrics['rubble_on_ice'] = np.sum(rubble_on_ice)
 
-        global_info['ice_transfered'] = 0
-        global_info['ore_transfered'] = 0
-        global_info['ice_mined'] = 0
-        global_info['ore_mined'] = 0
-        global_info['lichen_grown'] = 0
-        global_info['unit_created'] = 0
-        global_info['light_created'] = 0
-        global_info['heavy_created'] = 0
-        global_info['unit_destroyed'] = 0
-        global_info['light_destroyed'] = 0
-        global_info['heavy_destroyed'] = 0
+        return metrics
+    
+    def parse(self, game_state, metrics):
 
-        if prev_obs is not None:
-            global_info['ice_transfered'] = global_info['unit_ice'] - prev_obs['unit_ice']
-            global_info['ore_transfered'] = global_info['unit_ore'] - prev_obs['unit_ore']
-            global_info['ice_mined'] = global_info['total_ice'] - prev_obs['total_ice']
-            global_info['ore_mined'] = global_info['total_ore'] - prev_obs['total_ore']
-            global_info['lichen_grown'] = global_info['lichen_count'] - prev_obs['lichen_count']
-            global_info['unit_created'] = global_info['unit_count'] - prev_obs['unit_count']
-            global_info['light_created'] = global_info['light_count'] - prev_obs['light_count']
-            global_info['heavy_created'] = global_info['heavy_count'] - prev_obs['heavy_count']
-            global_info['unit_destroyed'] = prev_obs['unit_count'] - global_info['unit_count']
-            global_info['light_destroyed'] = prev_obs['light_count'] - global_info['light_count']
-            global_info['heavy_destroyed'] = prev_obs['heavy_count'] - global_info['heavy_count']
+        ## TOASK: should we add other metrics, in the other code we call the parent of IceRewardParser that takes in done
+        ## to give reward for winning, should we do the same here? Should we give for rubble destruction as well?
+        ## Are these weightings enough?
 
-        return global_info
+        final_reward = 0
+        reward_scale = 0.01
+        ice_norm = 1
+        step_weight_early = 1 + ((1000 - game_state.real_env_steps) / 1000) * 0.1
+
+
+        if self.prev_step_metrics is not None:
+            ice_dug_this_step = (metrics['ice_dug'] - self.prev_step_metrics['ice_dug']) / 4
+            ice_transfered_this_step = (metrics['ice_transferred'] - self.prev_step_metrics['ice_transferred']) / 4
+            water_increment_this_step = (metrics['water_produced'] - self.prev_step_metrics['water_produced'])
+
+            ice_dug_this_step_reward = ice_dug_this_step * reward_scale / ice_norm * step_weight_early
+            ice_transfered_this_step_reward = ice_transfered_this_step * reward_scale / ice_norm * step_weight_early
+            water_increment_this_step_reward = water_increment_this_step * reward_scale / 4 * step_weight_early
+
+            final_reward += ice_dug_this_step_reward
+            final_reward += ice_transfered_this_step_reward
+            final_reward += water_increment_this_step_reward
+
+        return final_reward
     
 
     @staticmethod