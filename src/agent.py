Fixed, eval, added 16 envs, ep limit to 256, eval to 1024, fixed engine run from model fil. Added seeding, Fixed controller, masking still not good.

src/agent.py
@@ -11,14 +11,18 @@ error e.g. print("message", file=sys.stderr)
 
 # pylint: disable=E0401
 import os.path as osp
+import seeding
 import numpy as np
 import torch as th
+import sys
 from sb3_contrib.ppo_mask import MaskablePPO
 from kit.config import EnvConfig
 from controller.controller import MultiUnitController
 from wrappers.obs_wrappers import SimpleUnitObservationWrapper
+from wrappers.utils import gaussian_ice_placement, bid_zero_to_not_waste
+from observation.observation_parser import FeatureParser
 
-MODEL_WEIGHTS_RELATIVE_PATH = "best_model"
+MODEL_WEIGHTS_RELATIVE_PATH = "logs/models/best_model"
 
 class Agent:
     """
@@ -31,13 +35,13 @@ class Agent:
         """
         self.player = player
         self.opp_player = "player_1" if self.player == "player_0" else "player_0"
-        np.random.seed(0)
+        seeding.set_seed(42)
         self.env_cfg: EnvConfig = env_cfg
 
         directory = osp.dirname(__file__)
         self.policy = MaskablePPO.load(osp.join(directory, MODEL_WEIGHTS_RELATIVE_PATH))
-
         self.controller = MultiUnitController(self.env_cfg)
+        self.observation_parser = FeatureParser()
 
     def bid_policy(self, step: int, obs, remainingOverageTime: int = 60):
         """
@@ -45,48 +49,14 @@ class Agent:
         and remaining overage time and returns a dictionary of bids for each resource
         """
 
-        return {"faction": 'AlphaStrike', "bid": 0}
+        return bid_zero_to_not_waste(self.player, obs)
 
     def factory_placement_policy(self, step: int, obs, remainingOverageTime: int = 60):
         """
         The factory placement policy is a function that takes in the current step, observation, \
         and remaining overage time and returns a dictionary of factory placement actions
         """
-
-        if obs["teams"][self.player]["metal"] == 0:
-            return {}
-        potential_spawns = list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1)))
-        potential_spawns_set = set(potential_spawns)
-        done_search = False
-
-        ice_diff = np.diff(obs["board"]["ice"])
-        pot_ice_spots = np.argwhere(ice_diff == 1)
-        if len(pot_ice_spots) == 0:
-            pot_ice_spots = potential_spawns
-        trials = 5
-        while trials > 0:
-            pos_idx = np.random.randint(0, len(pot_ice_spots))
-            pos = pot_ice_spots[pos_idx]
-
-            area = 3
-            for x_coord in range(area):
-                for y_coord in range(area):
-                    check_pos = [pos[0] + x_coord - area // 2, pos[1] + y_coord - area // 2]
-                    if tuple(check_pos) in potential_spawns_set:
-                        done_search = True
-                        pos = check_pos
-                        break
-                if done_search:
-                    break
-            if done_search:
-                break
-            trials -= 1
-        spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
-        if not done_search:
-            pos = spawn_loc
-
-        metal = obs["teams"][self.player]["metal"]
-        return {"spawn": pos, "metal": metal, "water": metal}
+        return gaussian_ice_placement(self.player, step, self.env_cfg, obs)
 
     def act(self, step: int, obs, remainingOverageTime: int = 60):
         """
@@ -95,10 +65,15 @@ class Agent:
         """
 
         raw_obs = {"player_0": obs, "player_1": obs}
-        obs = SimpleUnitObservationWrapper.convert_obs(raw_obs, env_cfg=self.env_cfg)
+        obs = SimpleUnitObservationWrapper.convert_obs(raw_obs, env_cfg=self.env_cfg, observation_parser=self.observation_parser)
         obs = obs[self.player]
 
-        obs = th.from_numpy(obs).float()
+        tensors = {}
+        for key, value in obs.items():
+            val = th.from_numpy(value).unsqueeze(0).float()
+            val = val.to('cuda')
+            tensors[key] = val
+        
         with th.no_grad():
 
             action_mask = (
@@ -107,23 +82,29 @@ class Agent:
                 .bool()
             )
 
-            features = self.policy.policy.features_extractor(obs.unsqueeze(0))
+            features = self.policy.policy.features_extractor(tensors)
             x_step = self.policy.policy.mlp_extractor.policy_net(features)
+            
+            batch_size, feature_dim, height, width = x_step.shape
+    
+            
+            x_step = x_step.permute(0, 2, 3, 1)
+            x_step = x_step.reshape(-1, feature_dim)
+
             logits = self.policy.policy.action_net(x_step)
+
+            action_mask = action_mask.permute(0, 2, 3, 1)
+            action_mask = action_mask.reshape(-1, feature_dim)
 
             logits[~action_mask] = -1e8
             dist = th.distributions.Categorical(logits=logits)
-            actions = dist.sample().cpu().numpy()
+
+            actions = dist.sample()
+            actions = actions.reshape(batch_size, height, width)
+            actions = actions.permute(0, 1, 2).cpu().numpy()
 
         lux_action = self.controller.action_to_lux_action(
             self.player, raw_obs, actions[0]
         )
-
-        shared_obs = raw_obs[self.player]
-        factories = shared_obs["factories"][self.player]
-        for unit_id in factories.keys():
-            factory = factories[unit_id]
-            if 1000 - step < 50 and factory["cargo"]["water"] > 100:
-                lux_action[unit_id] = 2
 
         return lux_action

src/controller/controller.py
@@ -7,6 +7,15 @@ import numpy as np
 import numpy.typing as npt
 from gymnasium import spaces
 
+move_deltas = np.array([[0, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
+factory_adjacent_delta_xy = np.array([
+    [-2, -1],
+    [-2, +0],
+    [-2, +1],
+])
+factory_adjacent_delta_xy = np.concatenate([factory_adjacent_delta_xy, -factory_adjacent_delta_xy])
+factory_adjacent_delta_xy = np.concatenate([factory_adjacent_delta_xy, factory_adjacent_delta_xy[:, ::-1]])
+
 class Controller:
     """
     A controller is a class that takes in an action space and converts it into a lux action
@@ -68,11 +77,20 @@ class MultiUnitController(Controller):
         see how the lux action space is defined in luxai_s2/spaces/action.py
 
         """
+        self.num_of_move = 0
+        self.num_of_transfer = 0
+        self.num_of_pickup = 0
+        self.num_of_dig = 0
+        self.num_of_recharge = 0
+        self.num_of_light_unit_build = 0
+        self.num_of_heavy_unit_build = 0
+        self.num_of_water_lichen = 0
+
 
         self.env_cfg = env_cfg
         self.move_act_dims = 4
         self.one_transfer_dim = 5
-        self.transfer_act_dims = self.one_transfer_dim * 2
+        self.transfer_act_dims = self.one_transfer_dim * 1
         self.pickup_act_dims = 1
         self.dig_act_dims = 1
         self.recharge_act_dims = 1
@@ -113,11 +131,11 @@ class MultiUnitController(Controller):
 
     def _get_transfer_action(self, id):
         
-        id = id - (self.transfer_act_dims/2)
+        id = id - (self.transfer_act_dims/1)
         id += 1
-        transfer_dir = id % (self.transfer_act_dims/2)
-        transfer_type_mapping = [0, 1]  # Mapping index to desired value
-        transfer_type_index = id // (self.transfer_act_dims/2)  # 0 for ice, 1 for ore
+        transfer_dir = id % (self.transfer_act_dims/1)
+        transfer_type_mapping = [0]  # Mapping index to desired value
+        transfer_type_index = id // (self.transfer_act_dims/1)  # 0 for ice, 1 for ore
         transfer_type = transfer_type_mapping[transfer_type_index.astype(int).item()]
         return np.array([1, transfer_dir, transfer_type, self.env_cfg.max_transfer_amount, 0, 1], dtype=int)
 
@@ -161,7 +179,7 @@ class MultiUnitController(Controller):
         """
         Checks if the action id corresponds to a light unit build action
         """
-        return id == self.light_unit_build_dim_high
+        return id < self.light_unit_build_dim_high
     
     def _get_light_unit_build_action(self, id):
         """
@@ -173,7 +191,7 @@ class MultiUnitController(Controller):
         """
         Checks if the action id corresponds to a heavy unit build action
         """
-        return id == self.heavy_unit_build_dim_high
+        return id < self.heavy_unit_build_dim_high
     
     def _get_heavy_unit_build_action(self, id):
         """
@@ -185,7 +203,7 @@ class MultiUnitController(Controller):
         """
         Checks if the action id corresponds to a water lichen action
         """
-        return id == self.water_lichen_dim_high
+        return id < self.water_lichen_dim_high
     
     def _get_water_lichen_action(self, id):
         """
@@ -202,11 +220,8 @@ class MultiUnitController(Controller):
 
         shared_obs = obs["player_0"]
         lux_action = {}
+
         units = shared_obs["units"][agent]
-
-        unit_id = list(units.keys())
-        factory_id = list(shared_obs["factories"][agent].keys())
-
         for unit_id, unit in units.items():
             pos = tuple(unit['pos'])
             filtered_action = action[pos]
@@ -233,12 +248,7 @@ class MultiUnitController(Controller):
             if not no_op:
                 lux_action[unit_id] = action_queue
 
-        
-
-        
-
         factories = shared_obs["factories"][agent]
-        
         for factory_id, factory in factories.items():
             pos = tuple(factory['pos'])
             filtered_action = action[pos]
@@ -251,6 +261,36 @@ class MultiUnitController(Controller):
                 lux_action[factory_id] = self._get_water_lichen_action(choice)
             else:
                 continue
+        
+        if lux_action:
+            for k, v in lux_action.items():
+                if isinstance(v, list):
+                    if v[0][0] == 0:
+                        self.num_of_move += 1
+                    elif v[0][0] == 1:
+                        self.num_of_transfer += 1
+                    elif v[0][0] == 2:
+                        self.num_of_pickup += 1
+                    elif v[0][0] == 3:
+                        self.num_of_dig += 1
+                    elif v[0][0] == 5:
+                        self.num_of_recharge += 1
+                else:
+                    if v == 0:
+                        self.num_of_light_unit_build += 1
+                    elif v == 1:
+                        self.num_of_heavy_unit_build += 1
+                    elif v == 2:
+                        self.num_of_water_lichen += 1
+
+        #print("Number of move actions: ", self.num_of_move)
+        #print("Number of transfer actions: ", self.num_of_transfer)
+        #print("Number of pickup actions: ", self.num_of_pickup)
+        #print("Number of dig actions: ",self. num_of_dig)
+        #print("Number of recharge actions: ", self.num_of_recharge)
+        #print("Number of light unit build actions: ", self.num_of_light_unit_build)
+        #print("Number of heavy unit build actions: ", self.num_of_heavy_unit_build)
+        #print("Number of water lichen actions: ",self. num_of_water_lichen)
 
         return lux_action
 
@@ -259,131 +299,162 @@ class MultiUnitController(Controller):
         Defines a simplified action mask for this controller's action space
         Doesn't account for whether robot has enough power
         """
+        # Get the own player and enemy
+        own = "player_0" if agent == "player_0" else "player_1"
+        enemy = "player_1" if agent == "player_0" else "player_0"
 
+        def factory_under_unit(unit_pos, factories):
+            for _, factory in factories.items():
+                factory_pos = factory["pos"]
+                if abs(unit_pos[0] - factory_pos[0]) <= 1 and abs(unit_pos[1] - factory_pos[1]) <= 1:
+                    return factory
+            return None
+        
+        # Get the shared observation
         shared_obs = obs[agent]
-        factory_occupancy_map = (
-            np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1
-        )
-        player_occupancy_map = (
-            np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1
-        )
+
+        # Create occupancy maps
+        factory_occupancy_map = (np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1)
+        player_occupancy_map = (np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1)
+
+        # Get the positions of the units
+        enemy_units = shared_obs["units"][enemy]
+        enemy_units_pos = [tuple(unit["pos"]) for unit in enemy_units.values()]
+        own_units = shared_obs["units"][own]
+        own_units_pos = [tuple(unit["pos"]) for unit in own_units.values()]
+        own_factories = shared_obs["factories"][own]     
+
+        # Fill the unit occupancy maps
         for player in shared_obs["units"]:
             for unit_id in shared_obs["units"][player]:
                 unit = shared_obs["units"][player][unit_id]
                 pos = np.array(unit["pos"])
                 player_occupancy_map[pos[0], pos[1]] = int(unit["unit_id"].split("_")[1])
-                
-        factories = {}
+
+        # Fill the factory occupancy map
         for player in shared_obs["factories"]:
-            factories[player] = {}
-            for unit_id in shared_obs["factories"][player]:
-                f_data = shared_obs["factories"][player][unit_id]
+            for f_id in shared_obs["factories"][player]:
+                f_data = shared_obs["factories"][player][f_id]
                 f_pos = f_data["pos"]
                 # store in a 3x3 space around the factory position it's strain id.
-                factory_occupancy_map[
-                    f_pos[0] - 1 : f_pos[0] + 2, f_pos[1] - 1 : f_pos[1] + 2
-                ] = f_data["strain_id"]
+                factory_occupancy_map[f_pos[0] - 1 : f_pos[0] + 2, f_pos[1] - 1 : f_pos[1] + 2] = f_data["strain_id"]
         
-        factories = shared_obs["factories"][agent]
-        units = shared_obs["units"][agent]
+        # Everything is invalid by default
         action_mask = np.zeros((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)
-        for unit_id in units.keys():
+
+        for factory_id, factory in own_factories.items():
+            x, y = factory["pos"]
+
+            unit_on_factory = (x, y) in own_units_pos
+
+            # Light unit build is valid only if there is no unit on the factory
+            if factory['cargo']['metal'] >= self.env_cfg.ROBOTS['LIGHT'].METAL_COST \
+                and factory['power'] >= self.env_cfg.ROBOTS['LIGHT'].POWER_COST \
+                and not unit_on_factory:
+                action_mask[self.light_unit_build_dim_high-self.light_unit_build_dim_high, x, y] = True
+
+            # Heavy unit build is valid only if there is no unit on the factory
+            if factory['cargo']['metal'] >= self.env_cfg.ROBOTS['HEAVY'].METAL_COST \
+                and factory['power'] >= self.env_cfg.ROBOTS['HEAVY'].POWER_COST \
+                and not unit_on_factory:
+                action_mask[self.heavy_unit_build_dim_high-self.heavy_unit_build_dim_high, x, y] = True
+
+            lichen_strains_size = np.sum(shared_obs["board"]["lichen"] == factory["strain_id"])
+            if factory["cargo"]["water"] >= (lichen_strains_size + 1) // self.env_cfg.LICHEN_WATERING_COST_FACTOR:
+                adj_xy = factory['pos'] + factory_adjacent_delta_xy
+                adj_xy = adj_xy[(adj_xy >= 0).all(axis=1) & (adj_xy < self.env_cfg.map_size).all(axis=1)]
+                adj_x, adj_y = adj_xy[:, 0], adj_xy[:, 1]
+                no_rubble = (shared_obs["board"]["rubble"][adj_x, adj_y] == 0)
+                no_ice = (shared_obs["board"]["ice"][adj_x, adj_y] == 0)
+                no_ore = (shared_obs["board"]["ore"][adj_x, adj_y] == 0)
+                if (no_rubble & no_ice & no_ore).any():
+                    action_mask[self.water_lichen_dim_high - self.water_lichen, x, y] = True
+
+        unit_move_targets = set()
+        sorted_units = sorted(shared_obs["units"][agent].items(), reverse=True, key=(lambda x: (x[1]["unit_type"] == "HEAVY", x[1]["cargo"]["ice"], x[1]["power"])))
+        for unit_id, unit in sorted_units:
             
-            # get position of unit
-            pos = np.array(unit["pos"])
+            x, y = np.array(unit["pos"])
+            if self.env_cfg.ROBOTS[unit["unit_type"]].ACTION_QUEUE_POWER_COST <= unit["power"]:
+                    for direction in range(len(move_deltas)):
+                        target_pos = (x + move_deltas[direction][0], y + move_deltas[direction][1])
 
-            # move is only valid if there is a unit there
-            action_mask[:4, pos[0], pos[1]] = False
+                        if (
+                            target_pos[0] < 0
+                            or target_pos[1] < 0
+                            or target_pos[0] >= self.env_cfg.map_size
+                            or target_pos[1] >= self.env_cfg.map_size
+                        ):
+                            continue
 
-            # transferring is valid only if the target exists
-            unit = units[unit_id]
-            # a[1] = direction (0 = center, 1 = up, 2 = right, 3 = down, 4 = left)
-            move_deltas = np.array([[0, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
-            for i, move_delta in enumerate(move_deltas):
-                transfer_pos = np.array(
-                    [pos[0] + move_delta[0], pos[1] + move_delta[1]]
-                )
-                # check if transfer position is valid
-                if (
-                    transfer_pos[0] < 0
-                    or transfer_pos[1] < 0
-                    or transfer_pos[0] >= len(factory_occupancy_map)
-                    or transfer_pos[1] >= len(factory_occupancy_map[0])
-                ):
-                    continue
+                        if factory_under_unit(target_pos, shared_obs["factories"][agent]) is not None:
+                            continue
 
-                # check if there is a unit there or factory
-                factory_there = factory_occupancy_map[transfer_pos[0], transfer_pos[1]]
-                unit_there = player_occupancy_map[transfer_pos[0], transfer_pos[1]]
+                        unit_at_target = player_occupancy_map[target_pos[0], target_pos[1]]
+                        if unit_at_target != -1:
+                            continue
 
-                if unit_there != -1:
-                    for j in range(3):  # we want to allow transfer of any kind of resouce if its possible
-                        index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
-                        action_mask[index, pos[0], pos[1]] = False
+                        if tuple(target_pos) in factory_occupancy_map:
+                            continue
 
-                if factory_there in shared_obs["teams"][agent]["factory_strains"]:
-                    for j in range(3):  # same for factories
-                        index = (self.transfer_dim_high - self.transfer_act_dims + i) + j * self.one_transfer_dim
-                        action_mask[index, pos[0], pos[1]] = False
+                        if not unit["unit_type"] != "HEAVY" and tuple(target_pos) in enemy_units_pos:
+                            continue
+                        
+                        if tuple(target_pos) not in unit_move_targets:
+                            action_mask[0:4, :, :] = True
+                            unit_move_targets.add(tuple(target_pos))
 
-            factory_there = factory_occupancy_map[pos[0], pos[1]]
-            on_top_of_factory = (
-                factory_there in shared_obs["teams"][agent]["factory_strains"]
-            )
 
-            if unit["cargo"]["ice"] == 0:
-                action_mask[
-                    self.move_dim_high : self.transfer_dim_high - 2* self.one_transfer_dim, pos[0], pos[1]
-                ] = False
+                        if unit["cargo"]["ice"] > 0:
+                            unit_has_ice = True
+                        else:
+                            unit_has_ice = False
 
-            if unit["cargo"]["ore"] == 0:
-                action_mask[
-                    self.move_dim_high + self.one_transfer_dim : self.transfer_dim_high - self.one_transfer_dim, pos[0], pos[1]
-                ] = False
+                        for direction in range(1, len(move_deltas)):
+                            transfer_pos = (x + move_deltas[direction][0], y + move_deltas[direction][1])
 
-            # if on the factory, or in the occumpancy map of factory, transfer of power is not valid
-            if on_top_of_factory or factory_there != -1:
-                action_mask[
-                    self.transfer_dim_high - self.one_transfer_dim : self.transfer_dim_high, pos[0], pos[1]
-                ] = True
-            # dig is valid only if on top of tile with rubble or resources or lichen
-            board_sum = (
-                shared_obs["board"]["ice"][pos[0], pos[1]]
-                + shared_obs["board"]["ore"][pos[0], pos[1]]
-                + shared_obs["board"]["rubble"][pos[0], pos[1]]
-                + shared_obs["board"]["lichen"][pos[0], pos[1]]
-            )
-            if board_sum > 0 and not on_top_of_factory:
-                action_mask[
-                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
-                ] = False
+                            if (
+                            transfer_pos[0] < 0
+                            or transfer_pos[1] < 0
+                            or transfer_pos[0] >= len(factory_occupancy_map)
+                            or transfer_pos[1] >= len(factory_occupancy_map[0])
+                            ):
+                                continue
 
-            # pickup is valid only if on top of factory tile
-            if on_top_of_factory:
-                action_mask[
-                    self.pickup_dim_high - self.pickup_act_dims : self.pickup_dim_high, pos[0], pos[1]
-                ] = False
-                action_mask[
-                    self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
-                ] = False
+                            if factory_under_unit(transfer_pos, shared_obs["factories"][agent]) is not None and unit_has_ice:
+                                action_mask[self.move_dim_high + direction, x, y] = True
+                            
 
-            # recharge should only be valid in the night
+                            unit_there = player_occupancy_map[transfer_pos[0], transfer_pos[1]]
+
+                            if unit_there != -1:
+                                if unit_has_ice:
+                                    action_mask[self.move_dim_high + direction, x, y] = True
+
+
+                        board_sum = (
+                            shared_obs["board"]["ice"][pos[0], pos[1]]
+                            + shared_obs["board"]["ore"][pos[0], pos[1]]
+                            + shared_obs["board"]["rubble"][pos[0], pos[1]]
+                            + shared_obs["board"]["lichen"][pos[0], pos[1]]
+                        )
+                        if board_sum > 0 and factory_under_unit((x, y), shared_obs["factories"][agent]) is None:
+                            action_mask[
+                                self.dig_dim_high - self.dig_act_dims : self.dig_dim_high, pos[0], pos[1]
+                            ] = True
+
+
+                        if factory_under_unit((x, y), shared_obs["factories"][agent]) is not None:
+                            action_mask[
+                                self.pickup_dim_high - self.pickup_act_dims : self.pickup_dim_high, pos[0], pos[1]
+                            ] = True
+
+            
             if shared_obs["real_env_steps"] % 40 > 30:
                 action_mask[
                     self.recharge_dim_high - self.recharge_act_dims : self.recharge_dim_high, pos[0], pos[1]
-                ] = False
+                ] = True
 
-            
-
-        
-
-
-        action_mask = np.ones((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)  
-
-        for factory_id in factories:
-            factory = factories[factory_id]
-            pos = np.array(factory["pos"])
-            action_mask[0:19, pos[0], pos[1]] = False
-
-        action_mask[4:14, :, :] = False
+        action_mask = np.ones((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)
+        action_mask[14, :, :] = False
         return action_mask

src/net/mixed_net.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from typing import Callable, List, Optional, Tuple, Type, Union
 from torch import Tensor
 import math
+import sys
 
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
 
@@ -397,7 +398,7 @@ class UNetWithResnet50Encoder(BaseFeaturesExtractor):
             nn.ConvTranspose2d(128, 50, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             # Final layer: Adjust channels to 25 without changing dimension
-            nn.ConvTranspose2d(50, 20, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(50, 15, kernel_size=3, stride=1, padding=1),
         )
         self.upconv_1 = nn.Conv2d(2048, 1024, kernel_size=1, stride=1)
         self.up_blocks = nn.ModuleList(up_blocks)

src/sb3_contrib/common/maskable/callbacks.py
@@ -34,11 +34,16 @@ class MaskableEvalCallback(EvalCallback):
     def __init__(self, *args, use_masking: bool = True, **kwargs):
         super().__init__(*args, **kwargs)
         self.use_masking = use_masking
+        self.ice_dug = []
+        self.ice_transfered = []
+        self.water_collected = []
 
     def _on_step(self) -> bool:
-        continue_training = True
 
-        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+        continue_training = True
+        
+        
+        if self.eval_freq > 0 and self.num_timesteps % self.eval_freq == 0:
             # Sync training and eval env if there is VecNormalize
             if self.model.get_vec_normalize_env() is not None:
                 try:
@@ -54,7 +59,7 @@ class MaskableEvalCallback(EvalCallback):
             self._is_success_buffer = []
 
             # Note that evaluate_policy() has been patched to support masking
-            episode_rewards, episode_lengths = evaluate_policy(
+            episode_rewards, episode_lengths, episode_ice_dug, episode_ice_transferred, episode_water_collected = evaluate_policy(
                 self.model,
                 self.eval_env,
                 n_eval_episodes=self.n_eval_episodes,
@@ -70,6 +75,9 @@ class MaskableEvalCallback(EvalCallback):
                 self.evaluations_timesteps.append(self.num_timesteps)
                 self.evaluations_results.append(episode_rewards)
                 self.evaluations_length.append(episode_lengths)
+                self.ice_dug.append(episode_ice_dug)
+                self.ice_transfered.append(episode_ice_transferred)
+                self.water_collected.append(episode_water_collected)
 
                 kwargs = {}
                 # Save success log if present
@@ -82,19 +90,37 @@ class MaskableEvalCallback(EvalCallback):
                     timesteps=self.evaluations_timesteps,
                     results=self.evaluations_results,
                     ep_lengths=self.evaluations_length,
+                    ice_dug=self.ice_dug,
+                    ice_transfered=self.ice_transfered,
+                    water_collected=self.water_collected,
                     **kwargs,
                 )
 
             mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
             mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+            mean_ice_dug, std_ice_dug = np.mean(episode_ice_dug), np.std(episode_ice_dug)
+            mean_ice_transferred, std_ice_transferred = np.mean(episode_ice_transferred), np.std(episode_ice_transferred)
+            mean_water_collected, std_water_collected = np.mean(episode_water_collected), np.std(episode_water_collected)
             self.last_mean_reward = mean_reward
-
-            if self.verbose > 0:
-                print(f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
-                print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
             # Add to current Logger
+
+            for i, env_ice in enumerate(self.ice_dug[-1]):
+                self.logger.record(f"eval/ice_dug_env_{i}", env_ice)
+
+            for i, env_ice in enumerate(self.ice_transfered[-1]):
+                self.logger.record(f"eval/ice_transfered_env_{i}", env_ice)
+
+            for i, env_water in enumerate(self.water_collected[-1]):
+                self.logger.record(f"eval/water_collected_env_{i}", env_water)
+
             self.logger.record("eval/mean_reward", float(mean_reward))
             self.logger.record("eval/mean_ep_length", mean_ep_length)
+            self.logger.record("eval/mean_ice_dug", mean_ice_dug)
+            self.logger.record("eval/std_reward", float(std_reward))
+            self.logger.record("eval/std_ice_transferred", float(std_ice_transferred))
+            self.logger.record("eval/mean_ice_transferred", mean_ice_transferred)
+            self.logger.record("eval/std_water_collected", float(std_water_collected))
+            self.logger.record("eval/mean_water_collected", mean_water_collected)
 
             if len(self._is_success_buffer) > 0:
                 success_rate = np.mean(self._is_success_buffer)

src/sb3_contrib/common/maskable/evaluation.py
@@ -79,6 +79,9 @@ def evaluate_policy(
     n_envs = env.num_envs
     episode_rewards = []
     episode_lengths = []
+    episode_ice_dug = []
+    episode_ice_transfered = []
+    episode_water_collected = []
 
     episode_counts = np.zeros(n_envs, dtype="int")
     # Divides episodes among different sub environments in the vector as evenly as possible
@@ -86,6 +89,7 @@ def evaluate_policy(
 
     current_rewards = np.zeros(n_envs)
     current_lengths = np.zeros(n_envs, dtype="int")
+
     observations = env.reset()
     states = None
     episode_starts = np.ones((env.num_envs,), dtype=bool)
@@ -107,6 +111,7 @@ def evaluate_policy(
                 deterministic=deterministic,
             )
         observations, rewards, dones, infos = env.step(actions)
+
         current_rewards += rewards
         current_lengths += 1
         for i in range(n_envs):
@@ -126,6 +131,7 @@ def evaluate_policy(
                         # the agent loses a life, but it does not correspond
                         # to the true end of episode
                         if "episode" in info.keys():
+                            
                             # Do not trust "done" with episode endings.
                             # Monitor wrapper includes "episode" key in info if environment
                             # has been wrapped with it. Use those rewards instead.
@@ -136,6 +142,9 @@ def evaluate_policy(
                     else:
                         episode_rewards.append(current_rewards[i])
                         episode_lengths.append(current_lengths[i])
+                        episode_ice_dug.append(info["metrics"]["ice_dug"])
+                        episode_ice_transfered.append(info["metrics"]["ice_transferred"])
+                        episode_water_collected.append(info["metrics"]["water_produced"])
                         episode_counts[i] += 1
                     current_rewards[i] = 0
                     current_lengths[i] = 0
@@ -145,8 +154,17 @@ def evaluate_policy(
 
     mean_reward = np.mean(episode_rewards)
     std_reward = np.std(episode_rewards)
+
+    mean_ice_dug = np.mean(episode_ice_dug)
+    mean_ice_transfered = np.mean(episode_ice_transfered)
+    mean_water_collected = np.mean(episode_water_collected)
+
+    std_ice_dug = np.std(episode_ice_dug)
+    std_ice_transfered = np.std(episode_ice_transfered)
+    std_water_collected = np.std(episode_water_collected)
+
     if reward_threshold is not None:
         assert mean_reward > reward_threshold, "Mean reward below threshold: " f"{mean_reward:.2f} < {reward_threshold:.2f}"
     if return_episode_rewards:
-        return episode_rewards, episode_lengths
-    return mean_reward, std_reward
+        return episode_rewards, episode_lengths, episode_ice_dug, episode_ice_transfered, episode_water_collected
+    return mean_reward, std_reward, mean_ice_dug, std_ice_dug, mean_ice_transfered, std_ice_transfered, mean_water_collected, std_water_collected

src/sb3_contrib/common/maskable/policies.py
@@ -136,7 +136,6 @@ class MaskableActorCriticPolicy(BasePolicy):
             pi_features, vf_features = features
             latent_pi: th.Tensor = self.mlp_extractor.forward_actor(pi_features)
             latent_vf: th.Tensor = self.mlp_extractor.forward_critic(vf_features)
-        # Evaluate the values for the given observations
 
         values = self.value_net(latent_vf)
 
@@ -148,10 +147,13 @@ class MaskableActorCriticPolicy(BasePolicy):
 
         batch_size, action_channels, height, width = latent_shape
 
-        latent_pi = latent_pi.view(-1, action_channels)
+        latent_pi = latent_pi.permute(0, 2, 3, 1)
+        latent_pi = latent_pi.reshape(-1, action_channels)
         assert latent_pi.shape[0] == batch_size * height * width
+        
 
         if action_masks is not None:
+            action_masks = action_masks.T
             action_masks = action_masks.reshape(-1, action_channels)
             assert latent_pi.shape == action_masks.shape
 
@@ -162,8 +164,11 @@ class MaskableActorCriticPolicy(BasePolicy):
         log_prob = distribution.log_prob(actions)
         
 
-        actions = actions.view(batch_size, height, width)
+        actions = actions.reshape(batch_size, height, width)
+        actions = actions.permute(0, 1, 2)
+   
         log_prob = log_prob.view(batch_size, height, width)
+        log_prob = log_prob.permute(0, 1, 2)
         log_prob = log_prob.sum(axis=[1,2])
 
         return actions, values, log_prob
@@ -278,7 +283,8 @@ class MaskableActorCriticPolicy(BasePolicy):
         """
         distribution, batch_size, height, width = self.get_distribution(observation, action_masks)
         actions = distribution.get_actions(deterministic=deterministic)
-        actions = actions.view(batch_size, height, width)
+        actions = actions.reshape(batch_size, height, width)
+        actions = actions.permute(0, 1, 2)
         return actions
 
     def predict(
@@ -358,7 +364,8 @@ class MaskableActorCriticPolicy(BasePolicy):
         if action_masks is not None:
             assert action_masks.shape == (batch_size * height * width, action_channels)
 
-        latent_pi = latent_pi.view(-1, action_channels)
+        latent_pi = latent_pi.permute(0, 2, 3, 1)
+        latent_pi = latent_pi.reshape(-1, action_channels)
         assert latent_pi.shape[0] == batch_size * height * width
 
         if action_masks is not None:
@@ -367,12 +374,13 @@ class MaskableActorCriticPolicy(BasePolicy):
         distribution = self._get_action_dist_from_latent(latent_pi)
         if action_masks is not None:
             distribution.apply_masking(action_masks)
+            
         actions = actions.view(-1)
         log_prob = distribution.log_prob(actions)
         
         log_prob = log_prob.view(batch_size, height, width)
+        log_prob = log_prob.permute(0, 1, 2)
         log_prob = log_prob.sum(axis=[1,2])
-
         values = self.value_net(latent_vf)
         return values, log_prob, distribution.entropy()
 
@@ -391,7 +399,10 @@ class MaskableActorCriticPolicy(BasePolicy):
             pi_features, _ = features
             latent_pi = self.mlp_extractor.forward_actor(pi_features)
         batch_size, action_channels, height, width = latent_pi.shape
-        latent_pi = latent_pi.view(-1, action_channels)
+
+
+        latent_pi = latent_pi.permute(0, 2, 3, 1)
+        latent_pi = latent_pi.reshape(-1, action_channels)
         assert latent_pi.shape[0] == batch_size * height * width
         distribution = self._get_action_dist_from_latent(latent_pi)
         if action_masks is not None: