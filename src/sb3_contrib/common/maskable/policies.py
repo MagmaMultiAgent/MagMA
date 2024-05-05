fixed, mixed_net, fixed value net, fixed masking cause it was still bad, net needs seeding

src/sb3_contrib/common/maskable/policies.py
@@ -163,10 +163,8 @@ class MaskableActorCriticPolicy(BasePolicy):
         actions = distribution.get_actions(deterministic=deterministic)
         log_prob = distribution.log_prob(actions)
         
-
         actions = actions.reshape(batch_size, height, width)
         actions = actions.permute(0, 1, 2)
-   
         log_prob = log_prob.view(batch_size, height, width)
         log_prob = log_prob.permute(0, 1, 2)
         log_prob = log_prob.sum(axis=[1,2])
@@ -374,8 +372,7 @@ class MaskableActorCriticPolicy(BasePolicy):
         distribution = self._get_action_dist_from_latent(latent_pi)
         if action_masks is not None:
             distribution.apply_masking(action_masks)
-            
-        actions = actions.view(-1)
+        
         log_prob = distribution.log_prob(actions)
         
         log_prob = log_prob.view(batch_size, height, width)

src/stable_baselines3/common/torch_layers.py
@@ -160,14 +160,19 @@ class CustomExtractor(nn.Module):
         super().__init__()
         device = get_device(device)
         policy_net: List[nn.Module] = [nn.Identity()]
-        value_net: List[nn.Module] = [nn.Flatten(start_dim=1)]
+        value_net: List[nn.Module] = [
+            nn.Conv2d(feature_dim, feature_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(feature_dim),
+            activation_fn(),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+        ]
 
         self.policy_net = nn.Sequential(*policy_net).to(device)
         self.value_net = nn.Sequential(*value_net).to(device)
 
-        # TODO: make these sizes dynamic
-        self.latent_dim_pi = feature_dim  # action space
-        self.latent_dim_vf = obs_shape ** 2 * feature_dim  # observation space
+        self.latent_dim_pi = feature_dim
+        self.latent_dim_vf = feature_dim
 
     def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
         """