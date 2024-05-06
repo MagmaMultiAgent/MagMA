52738dc4f9ea2d58b69b3ad27cc3ca622dc7fd10
Getlar <tamastheactual@gmail.com>
2024-05-06 11:40:37 +0200

    masking rev

--- src/parsers/action_parser_full_act.py
+++ src/parsers/action_parser_full_act.py
@@ -566,6 +566,8 @@ class ActionParser():
             "recharge": recharge_va,
             "do_nothing": do_nothing_va,
         })
+        for key in valid_actions:
+            valid_actions[key][:] = False
         return valid_actions
 
     def action_stats(self, player, actions, game_state: GameState):
